75bac09b5706c36a26c28d6ef2137932d846c226
bULLETHELL <polyswing7@hotmail.com>
2020-10-19 12:59:56 +0200

    consuming api

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+import requests
 
 # Create your views here.
 
 
 def homepage(request):
-    return HttpResponse("pythonprogramming.net homepage! Wow so #amaze.")
+    breeds_response = requests.get('https://dog.ceo/api/breeds/list/all')
+    breeds = breeds_response.json()
+
+    breed_selection_response = requests.get(
+        'https://dog.ceo/api/breed/hound/images')
+    breed_selection = breed_selection_response.json()
+
+    return render(request=request,
+                  template_name='main/home.html',
+                  context={'breeds': breeds['message'], 'breed_selection': breed_selection['message']})
